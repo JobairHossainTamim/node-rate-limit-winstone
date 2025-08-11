395e16d51b9b0ba600a9cd8fac33044c65a57646
JobairHossainTamim <likeabletamim@gmail.com>
2025-08-11 20:57:10 +0600

    node

---
+++ rateLimit.js
@@ -0,0 +1,23 @@
+const rateLimit = require("express-rate-limit");
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 10,
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  handler: (request, response, next, options) => {
+    if (request.rateLimit.used === request.rateLimit.limit) {
+      response.status(429).send({
+        error: "Too many requests, please try again later.",
+        rateLimit: {
+          limit: options.limit,
+          remaining: 0,
+          resetTime: new Date(Date.now() + options.windowMs).toISOString(),
+        },
+      });
+      response.status(options.statusCode).send(options.message);
+    }
+  },
+});
+
+module.exports = limiter;
